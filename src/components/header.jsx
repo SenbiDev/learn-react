adf5a506c2a181d7c67b7ca00608205faa6c7e7b
ralir13 <rali.ramdhani1313@gmail.com>
2025-08-03 18:03:01 +0700

    perbaiki header berita react

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,23 +7,19 @@ const Header = () => {
         <header className="bg-[#F2DE9F] w-full mb-[10px]">
             <div className="bg-[#F2DE9F] w-full h-[81px] flex items-center px-[20px] mb-[25px] box-border max-[529px]:h-full max-[529px]:flex-col max-[529px]:items-start max-[529px]:p-[10px_24px]">
 
-                {/* Search di kiri */}
                 <div className="flex-1">
                     <Search />
                 </div>
 
-                {/* "News" di tengah */}
                 <div className="flex-1 text-center">
                     <div className="text-black font-medium text-[32px] max-[529px]:text-[28px] max-[529px]:mb-[10px]">
                         News
                     </div>
                 </div>
 
-                {/* Login & Register di kanan */}
                 <div className="flex-1 flex justify-end gap-[20px]">
                     <LoginRegister />
                 </div>
-
             </div>
         </header>
     );
